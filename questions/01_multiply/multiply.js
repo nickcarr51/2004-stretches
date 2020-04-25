6230b6ce18c736f8655379ec8bcecd5133a1fbf4
nickcarr51 <nick.carr84@gmail.com>
2020-04-25 09:29:21 -0400

    stretch solution multiply

--- questions/01_multiply/multiply.js
+++ questions/01_multiply/multiply.js
@@ -1,5 +1,8 @@
 // This stretch requires you to write your own test cases
 
-const multiply = (num1, num2) => {};
+const multiply = (num1, num2) => {
+    if (Array.from(arguments).length !== 2) throw 'need two numbers';
+    return num1 * num2;
+};
 
 module.exports = { multiply };
--- questions/01_multiply/multiply.test.js
+++ questions/01_multiply/multiply.test.js
@@ -4,23 +4,23 @@ describe('multiply testing', () => {
   test('it should ensure it receives two numbers', () => {
     // test some values that should fail
 
-    expect(() => multiply(/*fill this in*/)).toThrow();
+    expect(() => multiply()).toThrow();
 
-    expect(() => multiply(/*fill this in*/)).toThrow();
+    expect(() => multiply(5)).toThrow();
   });
 
   test('it returns the product of two numbers', () => {
     // test the type of the returned value
-    expect(/*fill this in*/).toBe(/*fill this in*/);
+    expect(typeof multiply(2, 2)).toBe('number');
 
     // test that the returned value is correct
-    expect(/*fill this in*/).toBe(/*fill this in*/);
+    expect(() => multiply(2, 2)).toBe(4);
 
     // test some other values
-    expect(/* fill this in */).toBe(/* fill this in */);
+    expect(() => multiply(2, 3)).toBe(6);
   });
 
   test('it is not hardcoded (hint: use random numbers)', () => {
-    // test some random values
+    expect(() => multiply(5, 5)).toBe(25);
   });
 });
